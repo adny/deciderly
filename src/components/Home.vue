cefb86940191d6b85be27994798ab4c5ad095170
Andrew Ardener <a.ardener@gmail.com>
2017-11-19 23:49:00 +0100

    Adding store

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    Ask a Question!
+    <input v-model="question">
+  </div>
+</template>
+
+<script>
+  import store from '@/store'
+
+  export default {
+    data () {
+      return {
+        question: ''
+      }
+    },
+    store: store
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  modules: {
+    question: {
+      state: {
+        question: '',
+        answers: []
+      },
+      actions: {
+        addTodo: ({ commit }, question) => commit('ADD_QUESTION_MUTATION', question),
+        addAnswer: ({ commit }, answer) => commit('ADD_ANSWER_MUTATION', answer)
+      },
+      mutations: {
+        ADD_QUESTION_MUTATION (state, question) { state.question = question },
+        ADD_ANSWER_MUTATION (state, answer) { state.anwsers.push(answer) }
+      }
+    }
+  }
+})
